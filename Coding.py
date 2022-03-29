df7c21383ad85759cf6b430f3c8f7d414248cee1
Grretashalin <102421168+Grretashalin@users.noreply.github.com>
2022-03-28 21:24:20 -0700

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -1,3 +1,5 @@
+FIRST PROGRAM 
+
 from math import pi
 
 r = float(input ("Input the radius of the circle : "))
@@ -7,3 +9,17 @@ print ("Area of the circle is: " + str(pi * r**2))
 Output : 
 Input the radius of the circle : 1.1
 Area of the circle is: 3.8013271108436504
+  
+  
+  
+  
+SECOND PROGRAM
+  
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+Output:
+Input the Filename: abc.py                                                                                 
+The  extension of the file is : 'python'
+© 2022 GitHub, Inc.
