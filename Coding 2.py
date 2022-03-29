6a47257ead575312cb93a991c8d9cf2f98161dd3
Grretashalin <102421168+Grretashalin@users.noreply.github.com>
2022-03-28 21:19:17 -0700

    Created Coding 2.py
    Program to input a filename and print the extension

---
+++ Coding 2.py
@@ -0,0 +1,7 @@
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+Sample Output:
+Input the Filename: abc.py                                                                                 
+The  extension of the file is : 'python'
